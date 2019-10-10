d46fd3b61d1da992825e35713ca4fd01e84c6166
NikKnyshev <55587189+NikKnyshev@users.noreply.github.com>
2019-10-10 21:04:33 +0300

    the first laba
    all ok

---
+++ laba1.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+void main()
+{
+    float x,z;
+    printf ("VVedite x:");
+    scanf("%f",&x);
+    z=32*pow(x,6);
+    printf ("%f",z);
+    
+}
